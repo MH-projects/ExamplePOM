43c41d130727420ae99a3f54bfca7193487c344c
Juan Manuel Sandoval <hidalgo.10@hotmail.com>
2021-12-07 15:32:38 -0600

    library2

--- library/src/main/java/com/mh/library/ActLibrary.kt
+++ library/src/main/java/com/mh/library/ActLibrary.kt
@@ -1,12 +1,19 @@
 package com.mh.library
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.mh.library2.ActLibrary2
 
 class ActLibrary : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_library)
+
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+            startActivity(Intent(this, ActLibrary2::class.java))
+        }
     }
 }
---
+++ library2/src/main/java/com/mh/library2/ActLibrary2.kt
@@ -0,0 +1,12 @@
+package com.mh.library2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActLibrary2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_library2)
+    }
+}
